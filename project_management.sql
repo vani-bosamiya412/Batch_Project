32c860bfa5cd36e72af8c88fa793e366cb2f0d9a
VaniBosamiya <128339420+vani-bosamiya412@users.noreply.github.com>
2025-11-18 09:30:01 +0530

    Update project_management.sql

--- project_management.sql
+++ project_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2025 at 10:07 AM
+-- Generation Time: Nov 18, 2025 at 04:59 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -34,17 +34,14 @@ CREATE TABLE `batch_project` (
   `description` text NOT NULL,
   `type` enum('Web','Mobile Application') NOT NULL,
   `status` enum('Pending','Complete','On Hold','Continue') NOT NULL DEFAULT 'Pending',
+  `approved_by_admin` enum('Pending','Approved','Rejected') DEFAULT 'Pending',
+  `reason_for_hold` text DEFAULT NULL,
+  `admin_notes` text DEFAULT NULL,
   `members_names` text NOT NULL,
   `start_date` date NOT NULL,
-  `end_date` date NOT NULL
+  `end_date` date NOT NULL,
+  `completion_requested_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `batch_project`
---
-
-INSERT INTO `batch_project` (`id`, `client_name`, `title`, `description`, `type`, `status`, `members_names`, `start_date`, `end_date`) VALUES
-(2, 'a', 'a', 'abc', 'Mobile Application', 'Complete', 'abc, mno', '2025-11-10', '2026-01-01');
 
 -- --------------------------------------------------------
 
@@ -59,6 +56,20 @@ CREATE TABLE `batch_users` (
   `email` varchar(150) NOT NULL,
   `phone_number` varchar(15) NOT NULL,
   `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forgot_requests`
+--
+
+CREATE TABLE `forgot_requests` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `status` enum('pending','completed') NOT NULL DEFAULT 'pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -79,6 +90,13 @@ ALTER TABLE `batch_users`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `forgot_requests`
+--
+ALTER TABLE `forgot_requests`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -86,13 +104,19 @@ ALTER TABLE `batch_users`
 -- AUTO_INCREMENT for table `batch_project`
 --
 ALTER TABLE `batch_project`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `batch_users`
 --
 ALTER TABLE `batch_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `forgot_requests`
+--
+ALTER TABLE `forgot_requests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
